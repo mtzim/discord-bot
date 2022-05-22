ed928523b7c83c7eeddd117738a9b84426cc8962
Michael Zimmerman <mtzimmerman91@gmail.com>
2022-05-22 15:35:09 -0700

    Move main bot initialization into its own class

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
 import os
-import asyncio
 import logging
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
 
-# Create logging directory
-if not os.path.exists("./logs"):
-    os.makedirs("./logs")
+class MyBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+
+        if "intents" not in kwargs:
+            print(f"intents weren't found, loading from within class")
+            intents = discord.Intents.default()
+            intents.members = True
+            intents.message_content = True
+            kwargs["intents"] = intents
+
+        if "command_prefix" not in kwargs:
+            kwargs["command_prefix"] = "?"
+
+        super().__init__(*args, **kwargs)
+
+        self.initial_extensions = [
+            "cogs.shutdown",
+            "cogs.timezone_presence",
+        ]
+
+    async def setup_hook(self):
+        for ext in self.initial_extensions:
+            await self.load_extension(ext)
+        print(f"setup_hook running...")
+
+    async def close(self):
+        await super().close()
+
+    async def on_ready(self):
+        print(f"MyBot is ready!")
+
+    async def on_message(self, message):
+        if message.content == "$test":
+            await message.reply("I hear you!")
+        await super().process_commands(message)
+
 
 # Setup logging
+if not os.path.exists("./logs"):
+    os.makedirs("./logs")
 logger = logging.getLogger("discord")
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename="./logs/discord.log", encoding="utf-8", mode="w")
@@ -22,29 +56,9 @@ logger.addHandler(handler)
 # Load data from .env file
 load_dotenv()
 
-# Enable necessary Gateway features
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
-
-bot = commands.Bot(command_prefix="?", description="Nothing yet", intents=intents)
-
 # Changes to async in discord.py 2.0
 
-
-async def load_extensions():
-    for filename in os.listdir("./cogs"):
-        if filename.endswith(".py"):
-            # remove .py from end of file
-            await bot.load_extension(f"cogs.{filename[:-3]}")
-
-
-async def main():
-    token = os.getenv("DISCORD_TOKEN")
-    async with bot:
-        await load_extensions()
-        await bot.start(token)
-
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    token = os.getenv("DISCORD_TOKEN")
+    bot = MyBot()
+    bot.run(token)
