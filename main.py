2df95837af8adcc268ee83396354ceb1cc354bd2
Michael Zimmerman <mtzimmerman91@gmail.com>
2022-06-10 14:09:32 -0700

    Add command to change prefix per server

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import os
 import logging
-import discord
+from discord import Message, Intents, Guild
 from db_helper import SqlHelper as SQL
 from dotenv import load_dotenv
 from discord.ext import commands
 from customhelper import CustomHelpCommand
+from typing import Union, List
 
 
 class MyBot(commands.Bot):
     def __init__(self, *args, **kwargs):
 
         if "intents" not in kwargs:
-            intents = discord.Intents.all()
+            intents = Intents.all()
             # intents.members = True
             # intents.message_content = True
             kwargs["intents"] = intents
 
         if "command_prefix" not in kwargs:
-            kwargs["command_prefix"] = "?"
+            kwargs["command_prefix"] = self.get_prefix
 
         super().__init__(*args, help_command=CustomHelpCommand(), **kwargs)
         # super().__init__(*args, **kwargs)
@@ -28,6 +29,12 @@ class MyBot(commands.Bot):
             "cogs.utility",
             "cogs.moderation",
         ]
+
+    async def get_prefix(self, message: Message, /) -> Union[List[str], str]:
+        db = SQL("discord_bot_data")
+        prefix = db.get_prefix(message.guild.id)
+        db.close()
+        return prefix
 
     async def setup_hook(self):
         for ext in self.initial_extensions:
@@ -50,19 +57,19 @@ class MyBot(commands.Bot):
                 db.add_guild(guild.name, guild.id)
                 db.close()
 
-    async def on_guild_join(self, guild: discord.Guild):
+    async def on_guild_join(self, guild: Guild):
         """Adds the guild entry to the database when the bot joins the guild."""
         db = SQL("discord_bot_data")
         db.add_guild(guild.name, guild.id)
         db.close()
 
-    async def on_guild_remove(self, guild: discord.Guild):
+    async def on_guild_remove(self, guild: Guild):
         """Removes the guild entry from the database if the bot leaves or gets removed from the guild."""
         db = SQL("discord_bot_data")
         db.delete_guild(guild.id)
         db.close()
 
-    async def on_guild_update(self, before: discord.Guild, after: discord.Guild):
+    async def on_guild_update(self, before: Guild, after: Guild):
         """Updates the guild name in the database to match the server's guild name if it changes."""
         if before.name != after.name:
             db = SQL("discord_bot_data")
@@ -96,6 +103,16 @@ def load_commands(bot):
     @bot.command()
     async def ping(ctx: commands.Context):
         await ctx.reply(f"Pong! `{round(ctx.bot.latency*1000)}ms`")
+
+    @bot.command(help=f"Usage: `?prefix <NEW PREFIX>`")
+    @commands.has_guild_permissions(manage_guild=True)
+    async def prefix(ctx: commands.Context, new_prefix: str):
+        db = SQL("discord_bot_data")
+        if not db.set_prefix(ctx.guild.id, new_prefix):
+            await ctx.reply(f"Unable to change prefix.")
+        else:
+            await ctx.reply(f"Prefix successfully changed to `{new_prefix}`")
+        db.close()
 
     """ @bot.command()
     async def invite(ctx: commands.Context):
