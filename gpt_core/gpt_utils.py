356dc666eeee8ac055563c03b87b3099a967d48a
Michael Zimmerman <mtzimmerman91@gmail.com>
2023-05-08 01:48:21 -0700

    Update how prompt.txt is located

--- gpt_core/gpt_utils.py
+++ gpt_core/gpt_utils.py
@@ -1,3 +1,6 @@
+import os
+
+
 def open_file(filepath):
     with open(filepath, "r", encoding="utf-8") as file:
         return file.read()
@@ -8,8 +11,10 @@ def setup_personality():
     Setup the personality for the openai GPT model
     """
 
+    filepath = os.path.join(os.path.dirname(__file__), "prompt.txt")
+
     conversation = list()
-    conversation.append({"role": "system", "content": open_file("prompt.txt")})
+    conversation.append({"role": "system", "content": open_file(f"{filepath}")})
     conversation.append(
         {"role": "user", "content": "Who won the world series in 2020?"}
     )
