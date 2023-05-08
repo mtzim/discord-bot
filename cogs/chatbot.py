74921d37096b18a5e671eb2b06ef40670e5b0167
Michael Zimmerman <mtzimmerman91@gmail.com>
2023-05-08 07:11:27 -0700

    Add command to change chatbot prompt

--- cogs/chatbot.py
+++ cogs/chatbot.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from gpt_core.gpt_mode import ChatGPT
+from gpt_core.gpt_utils import setup_personality
 
 
 class ChatBot(commands.Cog):
@@ -32,6 +33,27 @@ class ChatBot(commands.Cog):
         # embed.add_field(name="\u2800", value=f"{response}", inline=False)
         # print(f"\n{response}\n")
         await interaction.followup.send(content=response)
+
+    @app_commands.command(
+        name="prompt",
+        description="Change the prompt of the chatbot",
+        extras=MODULE_NAME,
+    )
+    @app_commands.describe(input="[STRING] New prompt to set")
+    async def chatbot_prompt(self, interaction: discord.Interaction, input: str):
+        await interaction.response.defer(ephemeral=True)
+
+        # Check if the invoker is the bot owner
+        command_invoker = interaction.user.id
+        bot_owner = interaction.client.application.owner.id
+
+        if command_invoker == bot_owner:
+            self.chat_gpt.messages = setup_personality(input)
+            await interaction.followup.send(content="Prompt updated")
+        else:
+            await interaction.followup.send(
+                content="Only the bot owner can change the prompt"
+            )
 
 
 async def setup(bot):
--- gpt_core/gpt_utils.py
+++ gpt_core/gpt_utils.py
@@ -6,22 +6,26 @@ def open_file(filepath):
         return file.read()
 
 
-def setup_personality():
+def setup_personality(prompt=None):
     """
     Setup the personality for the openai GPT model
     """
 
-    filepath = os.path.join(os.path.dirname(__file__), "prompt.txt")
-
     conversation = list()
-    conversation.append({"role": "system", "content": open_file(f"{filepath}")})
-    conversation.append(
-        {"role": "user", "content": "Who won the world series in 2020?"}
-    )
-    conversation.append(
-        {
-            "role": "assistant",
-            "content": "The Los Angeles Dodgers won the World Series in 2020.",
-        }
-    )
+
+    if not prompt:
+        filepath = os.path.join(os.path.dirname(__file__), "prompt.txt")
+        conversation.append({"role": "system", "content": open_file(f"{filepath}")})
+        conversation.append(
+            {"role": "user", "content": "Who won the world series in 2020?"}
+        )
+        conversation.append(
+            {
+                "role": "assistant",
+                "content": "The Los Angeles Dodgers won the World Series in 2020.",
+            }
+        )
+    else:
+        conversation.append({"role": "system", "content": f"{prompt}"})
+
     return conversation
