b1452771e71825a89076194c1a52e7fe580585a0
Michael Zimmerman <mtzimmerman91@gmail.com>
2023-05-08 09:55:20 -0700

    Add command to configure GPT model settings

--- cogs/chatbot.py
+++ cogs/chatbot.py
@@ -1,5 +1,7 @@
 import os
+from typing import Optional
 import discord
+from discord.app_commands import Choice
 from discord.ext import commands
 from discord import app_commands
 from gpt_core.gpt_mode import ChatGPT
@@ -54,6 +56,96 @@ class ChatBot(commands.Cog):
             await interaction.followup.send(
                 content="Only the bot owner can change the prompt"
             )
+
+    gpt_settings_group = app_commands.Group(
+        name="modelsettings",
+        description="Manage GPT model settings",
+        extras=MODULE_NAME,
+    )
+
+    @gpt_settings_group.command(
+        name="set",
+        description="Set chatbot's GPT model settings",
+    )
+    @app_commands.describe(
+        conv_limit="[INT] Limit the model's context length",
+        model="[STRING] ID of the model to use",
+        temperature="[FLOAT] (0.0 to 2.0) Increase for output to be random, Decrease for output to be more deterministic",
+        max_tokens="[INT] (Less than 4096) The maximum number of tokens to generate in the chat completion",
+        pp="[FLOAT] (-2.0 to 2.0) Increase or Decrease model's likelihood to talk about new topics",
+        fp="[FLOAT] (-2.0 to 2.0) Increase or Decrease model's likelihood to repeat the same line verbatim",
+        stop="[STRING] Up to 4 sequences where the API will stop generating further tokens",
+    )
+    @app_commands.choices(
+        model=[
+            Choice(name="gpt-4", value=1),
+            Choice(name="gpt-4-0314", value=2),
+            Choice(name="gpt-4-32k", value=3),
+            Choice(name="gpt-4-32k-0314", value=4),
+            Choice(name="gpt-3.5-turbo", value=5),
+            Choice(name="gpt-3.5-turbo-0301", value=6),
+        ]
+    )
+    async def set_chatbot_parameters(
+        self,
+        interaction: discord.Interaction,
+        conv_limit: Optional[int] = None,
+        model: Optional[Choice[int]] = None,
+        temperature: Optional[float] = None,
+        max_tokens: Optional[int] = None,
+        pp: Optional[float] = None,
+        fp: Optional[float] = None,
+        stop: Optional[str] = None,
+    ):
+        updated = False
+        if conv_limit is not None:
+            self.chat_gpt.conversation_limit = conv_limit
+            updated = True
+        if model is not None:
+            self.chat_gpt.model = model.name
+            updated = True
+        if temperature is not None and temperature >= 0 and temperature <= 2.0:
+            self.chat_gpt.temperature = temperature
+            updated = True
+        if max_tokens is not None and max_tokens <= 4096:
+            self.chat_gpt.max_tokens = max_tokens
+            updated = True
+        if pp is not None and pp >= -2.0 and pp <= 2.0:
+            self.chat_gpt.presence_penalty = pp
+            updated = True
+        if fp is not None and fp >= -2.0 and fp <= 2.0:
+            self.chat_gpt.frequency_penalty = fp
+            updated = True
+        if stop is not None:
+            self.chat_gpt.stop = stop
+            updated = True
+
+        if updated:
+            await interaction.response.send_message(
+                content="Model settings updated", ephemeral=True
+            )
+        else:
+            await interaction.response.send_message(
+                content="No model settings were changed, select a parameter to update",
+                ephemeral=True,
+            )
+
+    @gpt_settings_group.command(
+        name="view",
+        description="View chatbot's GPT model settings",
+    )
+    async def view_chatbot_parameters(self, interaction: discord.Interaction):
+        conv_limit = self.chat_gpt.conversation_limit
+        model = self.chat_gpt.model
+        temperature = self.chat_gpt.temperature
+        max_tokens = self.chat_gpt.max_tokens
+        pp = self.chat_gpt.presence_penalty
+        fp = self.chat_gpt.frequency_penalty
+        stop = self.chat_gpt.stop
+
+        await interaction.response.send_message(
+            content=f"Conversation limit: `{conv_limit}`\nModel: {model}\nTemperature: `{temperature}`\nMax tokens: `{max_tokens}`\nPresence penalty: `{pp}`\nFrequency penalty: `{fp}`\nStop: `{stop}`"
+        )
 
 
 async def setup(bot):
--- gpt_core/gpt_mode.py
+++ gpt_core/gpt_mode.py
@@ -17,14 +17,6 @@ class ChatGPT:
         Interacts with the user, taking their input and displaying the bot's response
     """
 
-    CONVERSATION_LIMIT = 20
-    MODEL = "gpt-3.5-turbo"  # gpt-4, gpt-4-0314, gpt-4-32k, gpt-4-32k-0314, gpt-3.5-turbo, gpt-3.5-turbo-0301
-    TEMPERATURE = 0.6  # defaults to 1 (0.0 to 2.0)
-    MAX_TOKENS = 256  # defaults to  inf
-    PRESENCE_PENALTY = 0  # defaults to 0 (-2.0 to 2.0)
-    FREQUENCY_PENALTY = 0  # defaults to 0 (-2.0 to 2.0)
-    STOP = None  # defaults to None
-
     def __init__(self) -> None:
         # GPT Setup
         load_dotenv()
@@ -33,15 +25,26 @@ class ChatGPT:
         # Setup Personality
         self.messages = gpt_utils.setup_personality()
 
+        # Limit context length
+        self.conversation_limit = 20
+
+        # Configure GPT model parameters
+        self.model = "gpt-3.5-turbo"  # gpt-4, gpt-4-0314, gpt-4-32k, gpt-4-32k-0314, gpt-3.5-turbo, gpt-3.5-turbo-0301
+        self.temperature = 0.6  # defaults to 1 (0.0 to 2.0)
+        self.max_tokens = 256  # defaults to  inf
+        self.presence_penalty = 0  # defaults to 0 (-2.0 to 2.0)
+        self.frequency_penalty = 0  # defaults to 0 (-2.0 to 2.0)
+        self.stop = None  # defaults to None
+
     def ask_openai(self):
         completion = completions_with_backoff(
-            model=self.MODEL,
+            model=self.model,
             messages=self.messages,
-            temperature=self.TEMPERATURE,
-            max_tokens=self.MAX_TOKENS,
-            presence_penalty=self.PRESENCE_PENALTY,
-            frequency_penalty=self.FREQUENCY_PENALTY,
-            stop=self.STOP,
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
+            presence_penalty=self.presence_penalty,
+            frequency_penalty=self.frequency_penalty,
+            stop=self.stop,
         )
         self.messages.append(completion["choices"][0]["message"])
         return completion["choices"][0]["message"]["content"]
@@ -55,7 +58,7 @@ class ChatGPT:
         response = self.ask_openai()
 
         # Limit conversation while keeping system prompt
-        if len(self.messages) >= self.CONVERSATION_LIMIT:
+        if len(self.messages) >= self.conversation_limit:
             self.messages[2] = self.messages[0]
             self.messages = self.messages[2:]
 
